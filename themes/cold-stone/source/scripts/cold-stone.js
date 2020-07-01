ec338137a247f10aff51d894198f7a0d90e69857
Cold Stone <xiaoranran1993@outlook.com>
2020-07-01 10:54:29 +0800

    feat: add mouse down event

--- themes/cold-stone/source/scripts/cold-stone.js
+++ themes/cold-stone/source/scripts/cold-stone.js
@@ -214,3 +214,25 @@ if (article) {
 //   const path = wave.getElementsByTagName('path')[0]
 //   path.setAttribute('fill', random)
 // }
+
+// 鼠标按下事件监听
+document.addEventListener('mousedown', function (event) {
+  var target = event.target;
+  var body = document.body;
+  var html = document.documentElement;
+
+  // 设置自定义属性值
+  body.style.setProperty('--pagex', event.pageX);
+  body.style.setProperty('--pagey', event.pageY);
+
+  html.style.setProperty('--clientx', event.clientX);
+  html.style.setProperty('--clienty', event.clientY);
+  html.style.setProperty('--scrolly', window.pageYOffset);
+
+  target.style.setProperty('--offsetx', event.offsetX);
+  target.style.setProperty('--offsety', event.offsetY);
+  target.parentElement.style.setProperty('--target-width', target.clientWidth);
+  target.parentElement.style.setProperty('--target-height', target.clientHeight);
+  target.parentElement.style.setProperty('--target-left', target.offsetLeft);
+  target.parentElement.style.setProperty('--target-top', target.offsetTop);
+});
