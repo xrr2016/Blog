b69b0b38e5fe3cb7e5c3116c0a24e4a76b063610
Cold Stone <xiaoranran1993@outlook.com>
2021-05-20 19:31:25 +0800

    feat: update index

--- themes/cold-stone/source/index.js
+++ themes/cold-stone/source/index.js
@@ -2,3 +2,23 @@
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
+const backgrounds = [
+  '#ffffff',
+  '#fafafa',
+  '#fafaf0',
+  '#f0fef6',
+  '#f0fef6',
+  '#fff0f0',
+]
+
+let index = 0
+let root = document.documentElement
+
+document.getElementById('color').addEventListener('click', () => {
+  index++
+  if (index >= backgrounds.length) {
+    index = 0
+  }
+  console.log(index)
+  root.style.setProperty('--bg-color', backgrounds[index])
+})
